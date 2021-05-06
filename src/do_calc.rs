9a7a560c812aaaceb53ebe9676d19c5c372aa9f1
Arthur Melton <amtitan@icloud.com>
2021-05-06 08:20:46 -0400

    Working! (only *, /, -, +)

--- src/do_calc.rs
+++ src/do_calc.rs
@@ -10,12 +10,71 @@ pub fn calc(input: &String) {
     re = Regex::new(r"[-]").unwrap();
     let add = re.replace_all(&add, "|-|");
     let mut split: Vec<&str> = add.split("|").collect();
-    for x in 0..split.len() {
-        if (split[x] == "*") {
+    let mut length = split.len();
+    let mut x = 0;
+    while x < length {
+        if split[x] == "*" {
             split[x] = Box::leak((split[x-1].parse::<i32>().unwrap() * split[x+1].parse::<i32>().unwrap()).to_string().into_boxed_str());
-            
+            split[x-1] = "";
+            split[x+1] = "";
+            let mut new: Vec<&str> = vec![];
+            for y in 0..split.len() {
+                if split[y] != "" {
+                    new.push(split[y]);
+                }
+            }
+            split = new;
+            x = 0;
+            length = split.len();
+        }else if split[x] == "/" {
+            split[x] = Box::leak((split[x-1].parse::<i32>().unwrap() / split[x+1].parse::<i32>().unwrap()).to_string().into_boxed_str());
+            split[x-1] = "";
+            split[x+1] = "";
+            let mut new: Vec<&str> = vec![];
+            for y in 0..split.len() {
+                if split[y] != "" {
+                    new.push(split[y]);
+                }
+            }
+            split = new;
+            x = 0;
+            length = split.len();
+        }else {
+            x = x+1;
         }
     }
-    println!("num : {:?}", split);
-    print!("calc (do ctrl+c or cmd+c to stop): {:?}", &split);
+    length = split.len();
+    x = 0;
+    while x < length {
+        if split[x] == "+" {
+            split[x] = Box::leak((split[x-1].parse::<i32>().unwrap() + split[x+1].parse::<i32>().unwrap()).to_string().into_boxed_str());
+            split[x-1] = "";
+            split[x+1] = "";
+            let mut new: Vec<&str> = vec![];
+            for y in 0..split.len() {
+                if split[y] != "" {
+                    new.push(split[y]);
+                }
+            }
+            split = new;
+            x = 0;
+            length = split.len();
+        }else if split[x] == "-" {
+            split[x] = Box::leak((split[x-1].parse::<i32>().unwrap() - split[x+1].parse::<i32>().unwrap()).to_string().into_boxed_str());
+            split[x-1] = "";
+            split[x+1] = "";
+            let mut new: Vec<&str> = vec![];
+            for y in 0..split.len() {
+                if split[y] != "" {
+                    new.push(split[y]);
+                }
+            }
+            split = new;
+            x = 0;
+            length = split.len();
+        }else {
+            x = x+1;
+        }
+    }
+    print!("calc (do ctrl+c or cmd+c to stop): {:?}", &split[0]);
 }
