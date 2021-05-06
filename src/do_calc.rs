14b93f0d53f7f4d63bf114763c7adaeacacd4964
Arthur Melton <amtitan@icloud.com>
2021-05-05 23:27:08 -0400

    start to calc

--- src/do_calc.rs
+++ src/do_calc.rs
@@ -1,4 +1,22 @@
+extern crate regex;
+use regex::Regex;
 pub fn calc(input: &String) {
-    let awn = input;
-    print!("calc: {:?}", awn);
+    let mut re = Regex::new(r"[*]").unwrap();
+    let multi = re.replace_all(&input, "|*|");
+    re = Regex::new(r"[/]").unwrap();
+    let divide = re.replace_all(&multi, "|/|");
+    re = Regex::new(r"[+]").unwrap();
+    let add = re.replace_all(&divide, "|+|");
+    re = Regex::new(r"[-]").unwrap();
+    let add = re.replace_all(&add, "|-|");
+    let mut split: Vec<&str> = add.split("|").collect();
+    //split.last().unwrap() = split.last().unwrap().take();
+    println!("num : {:?}",  split);
+    for x in 0..split.len() {
+        if (split[x] == "*") {
+            split[x] = Box::leak((split[x-1].parse::<i32>().unwrap() * split[x+1].parse::<i32>().unwrap()).to_string().into_boxed_str());
+        }
+    }
+    println!("num : {:?}", split);
+    print!("calc (do ctrl+c or cmd+c to stop): {:?}", &split);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod do_calc;
 use std::env;
 fn main() {
-    let mut line = String::new();
-    println!("Enter you calculation:");
-    std::io::stdin().read_line(&mut line);
-    do_calc::calc(&line);
+    loop {
+        let mut line = String::new();
+        println!("");
+        println!("Enter you calculation:");
+        std::io::stdin().read_line(&mut line);
+        do_calc::calc(&line);
+    }
 }
